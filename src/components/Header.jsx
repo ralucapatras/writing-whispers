b9e0a1d96c8d2d5d82fcbd92992b5521d0979683
ralucapatras <ralucap98@gmail.com>
2023-09-08 14:28:10 +0100

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+//import React from "react";
+import React from "react";
+import NotesIcon from "@mui/icons-material/Notes";
+//import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {/* <HighlightIcon /> */}
+        <NotesIcon />
+        Writing Whispers
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
